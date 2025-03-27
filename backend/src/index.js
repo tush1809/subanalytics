1b22a43e495d60b60ceeeb81582ff720540872ee
Tushar Srivastava <tusharsrivastava2003@gmail.com>
2025-03-28 00:51:32 +0530

    Change default server port from 5000 to 8000

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,5 +25,5 @@ connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true
   .catch(err => console.error(err));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
