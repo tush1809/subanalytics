485722f5ed19bbdec2cd8d681e5caa80fa49dd90
Utkarsh601 <utkarshmishra6102@gmail.com>
2025-03-27 22:44:04 +0530

    "defaultpage"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Route  , Routes} from "react-router-dom";
+import { Route  , Routes , Navigate} from "react-router-dom";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -12,6 +12,7 @@ function App() {
     <>
       
         <Routes>
+          <Route path="/" element={<Navigate to="/signup" />} /> 
           <Route path="/dashboard" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/Signup" element={<Signup/>}/>
